9c043cd27497dad1a92e0dd68c38e21870aaa3d2
Brando Vidal <yisusdracon@gmail.com>
2023-09-30 18:19:51 -0500

    ⚒  chore: edit color pointer

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -28,7 +28,7 @@ const FollowMouse = () => {
       <div
         style={{
           position: 'absolute',
-          backgroundColor: '#89f',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
           border: '1px solid #fff',
           borderRadius: '50%',
           opacity: 0.8,
