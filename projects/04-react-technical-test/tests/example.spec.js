db6eab17bbf32d4b4f4932a0c95b2f12acc82309
Brando Vidal <yisusdracon@gmail.com>
2023-10-01 17:41:35 -0500

    ⚒  chore: implement test

--- projects/04-react-technical-test/tests/example.spec.js
+++ projects/04-react-technical-test/tests/example.spec.js
@@ -1,19 +1,18 @@
 // @ts-check
 import { test, expect } from '@playwright/test'
-import { CAT_ENDPOINT_RANDOM_FACT_URL } from '../src/contants'
+import { CAT_ENDPOINT_IMAGE_URL } from '../src/contants'
 
 const LOCALHOST_URL = 'http://localhost:5173'
 
 test('has title', async ({ page }) => {
   await page.goto(LOCALHOST_URL)
 
-  // Expect a title "to contain" a substring.
-  const text = await page.getByRole('paragraph')
-  const image = await page.getByRole('img')
+  const text = page.getByRole('paragraph')
+  const image = page.getByRole('img')
 
   const textContent = await text.textContent()
   const imageSrc = await image.getAttribute('src')
 
-  await expect(textContent?.length).toBeGreaterThan(0)
-  await expect(imageSrc?.startsWith(CAT_ENDPOINT_RANDOM_FACT_URL)).toBeTruthy()
+  expect(textContent?.length).toBeGreaterThan(0)
+  expect(imageSrc?.startsWith(CAT_ENDPOINT_IMAGE_URL)).toBeTruthy()
 })
