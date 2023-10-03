0ec844987c81f0c95def38a2d95798d2caaee594
Brando Vidal <yisusdracon@gmail.com>
2023-10-02 22:22:50 -0500

    ⚒  submit form values

--- projects/05-react-search-page/src/App.jsx
+++ projects/05-react-search-page/src/App.jsx
@@ -1,19 +1,41 @@
-// import './App.css'
+import { useRef } from 'react'
+
+import './App.css'
+
+import { Movies } from './components/movies'
+
+import { useMovies } from './hooks/useMovies'
 
 function App () {
+  const { movies } = useMovies()
+  const inputRef = useRef()
+
+  const handleClick = event => {
+    event.preventDefault()
+
+    const form = Object.fromEntries(new window.FormData(event.target))
+    // const value = form.get('query')
+    console.log({ form })
+  }
+
   return (
     <div className='page'>
       <header>
         <h1>Search App</h1>
-        <form action='' className='form'>
-          <input type='text' placeholder='Avengers, Star wars, Interstelar' />
 
+        <form className='form' onSubmit={handleClick}>
+          <input
+            ref={inputRef}
+            name='query'
+            type='text'
+            placeholder='Avengers, Star wars, Interstelar'
+          />
           <button type='submit'>Search</button>
         </form>
       </header>
 
       <main>
-        results
+        <Movies movies={movies} />
       </main>
     </div>
   )
