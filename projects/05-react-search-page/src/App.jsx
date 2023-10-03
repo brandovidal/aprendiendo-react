1cf0f9c37dd1c0349bf5c09c4f33a168f2e9fb58
Brando Vidal <yisusdracon@gmail.com>
2023-10-02 20:57:20 -0500

    ⚒  chore: start project

---
+++ projects/05-react-search-page/src/App.jsx
@@ -0,0 +1,22 @@
+// import './App.css'
+
+function App () {
+  return (
+    <div className='page'>
+      <header>
+        <h1>Search App</h1>
+        <form action='' className='form'>
+          <input type='text' placeholder='Avengers, Star wars, Interstelar' />
+
+          <button type='submit'>Search</button>
+        </form>
+      </header>
+
+      <main>
+        results
+      </main>
+    </div>
+  )
+}
+
+export default App
