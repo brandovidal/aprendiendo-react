13899ccc1ee4a9675f2d51d936368387452e6771
Brando Vidal <yisusdracon@gmail.com>
2023-10-16 23:35:46 -0500

    ⚒  implement SPA

--- projects/07-brandovidal-router/README.md
+++ projects/07-brandovidal-router/README.md
@@ -1,8 +1,8 @@
 # Crea un React Router desde cero
 
 - ✅ Instalar el linter
-- [ ] Crear una forma de hacer MPAs (Multiple Page Application)
-- [ ] Crea una forma de hacer SPAs (Single Page Applications)
+- ✅ Crear una forma de hacer MPAs (Multiple Page Application)
+- ✅ Crea una forma de hacer SPAs (Single Page Applications)
 - [ ] Poder navegar entre páginas con el botón de atrás
 - [ ] Crear componente Link para hacerlo declarativo
 - [ ] Crear componente Router para hacerlo más declarativo
--- projects/07-brandovidal-router/src/App.jsx
+++ projects/07-brandovidal-router/src/App.jsx
@@ -1,10 +1,62 @@
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
-function App () {
+const NAVIGATION_EVENT = 'pushState'
+
+function navigate (href) {
+  window.history.pushState({}, '', href)
+
+  const navigationEvent = new Event(NAVIGATION_EVENT)
+  window.dispatchEvent(navigationEvent)
+}
+
+function HomePage () {
   return (
     <>
-      <h1>brandoVidal Router</h1>
+      <h1>Home Page</h1>
+      <p>This is the home page</p>
+      <button onClick={() => navigate('/about')}>Ir a About</button>
     </>
+  )
+}
+
+function AboutPage () {
+  return (
+    <>
+      <h1>About Page</h1>
+      <div>
+        <p>This is the about page</p>
+        <img
+          src='https://avatars.githubusercontent.com/u/65886822?v=4'
+          alt='brandovidal'
+        />
+      </div>
+      <button onClick={() => navigate('/')}>Ir a Home</button>
+    </>
+  )
+}
+
+function App () {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+    }
+  }, [])
+
+  return (
+    <main>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
+    </main>
   )
 }
 
